a40c15e6160d9ab0eef738d46d81eef696e3338f
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-02 11:29:25 +0530

    feat : added score checker

--- script.js
+++ script.js
@@ -14,12 +14,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct answer';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = ' Too High';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lose';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = ' Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lose';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
